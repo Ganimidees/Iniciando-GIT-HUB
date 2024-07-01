425ac2466e78c972153fbd925ffc064556662fe8
Maximiliano Pino <maximiliano.pino_m@mail.udp.cl>
2024-07-01 17:56:12 -0400

    nueva funcion multiplicar

--- hola.py
+++ hola.py
@@ -4,6 +4,15 @@ def fn_suma(a,b):
 
 if __name__ == "__main__":
     a = input("Ingrese el numero: ")
-    b = input("Ingrese el numero:2 ")
+    b = input("Ingrese el otro  : ")
     resultado = fn_suma(int(a),int(b))
+    print("La suma es: ", resultado)
+
+
+def multiplicar (a,b):
+    total_mult = a * b
+    return total_mult
+
+if __name__ == "__main__":
+    resultado = multiplicar(int(a),int(b))
     print("La suma es: ", resultado)
